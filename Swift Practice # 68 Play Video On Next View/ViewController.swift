078e3ac3bacdf23793f728a9bb664da876ba1b79
Dogpa <dogpa0130@gmail.com>
2021-08-28 10:53:11 +0800

    Initial Commit

---
+++ Swift Practice # 68 Play Video On Next View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 68 Play Video On Next View
+//
+//  Created by Dogpa's MBAir M1 on 2021/8/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
